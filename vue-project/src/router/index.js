5c6a2b8b04122aca71b608f6a9d696de598f58bc
Nawal69200 <nawal.kajtou@gmail.com>
2025-07-26 19:35:57 +0200

    inintial commit

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,11 +1,11 @@
+import App from '@/App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: HomeView
+    component: App
   },
   // Tu peux ajouter d'autres routes ici plus tard
 ]
